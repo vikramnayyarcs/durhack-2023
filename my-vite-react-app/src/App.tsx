e8beefab31861ed7cbd28fcee6578ba97632aac7
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2023-11-04 19:26:31 +0000

    Validate if empty otherwise store text content of file.

--- my-vite-react-app/src/App.tsx
+++ my-vite-react-app/src/App.tsx
@@ -1,34 +1,12 @@
 import "./App.css";
-import { useState } from "react";
+import FormComponent from "./components/FormComponent";
 
 function App() {
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [fileContent, setFileContent] = useState("");
-
-  // Function to handle file input changes
-  const handleFileInputChange = (e) => {
-    const file = e.target.files[0]; // Get the first selected file
-
-    // Set the selected file in state
-    setSelectedFile(file);
-
-    // Read the contents of the selected file
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      const content = event.target?.result;
-      setFileContent(content);
-    };
-
-    reader.readAsText(file);
-  };
-
-  console.log(fileContent);
-
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="flex flex-col space-y-28">
         <div className="flex items-center">
-          <input type="file" onChange={handleFileInputChange} />
+          <FormComponent/>
 
         </div>
       </div>
---
+++ my-vite-react-app/src/components/FormComponent.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  file: any;
+  prompt: string;
+};
+
+export default function FormComponent() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    // Access the file content here, assuming you have a state variable for it.
+    console.log("File content:", fileContent);
+    console.log(data);
+  };
+
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [fileContent, setFileContent] = useState("");
+
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const content = event.target.result;
+      setFileContent(content);
+    };
+
+    reader.readAsText(file);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="w-1/2 mx-auto mt-8">
+      <div className="mb-4">
+        <label htmlFor="file" className="block font-medium text-gray-700">
+          Select a File
+        </label>
+        <input
+          type="file"
+          {...register("file", { required: "File is required" })}
+          onChange={(e) => handleFileInputChange(e)}
+          className="mt-1 p-2 border border-gray-300 rounded w-full"
+        />
+        {errors.file && (
+          <p className="text-red-500 text-sm">{errors.file.message}</p>
+        )}
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="prompt" className="block font-medium text-gray-700">
+          Input Music Data for Analysis
+        </label>
+        <input
+          type="text"
+          id="prompt"
+          {...register("prompt")}
+          placeholder="Type here..."
+          className="mt-1 p-2 border border-gray-300 rounded w-full"
+        />
+      </div>
+
+      <div className="text-center">
+        <button
+          type="submit"
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+}
