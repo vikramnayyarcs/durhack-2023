a7153df13190052f3bfa9b74cffc3917d8d10936
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2023-11-04 15:36:19 +0000

    Blank results show.

--- my-vite-react-app/src/App.tsx
+++ my-vite-react-app/src/App.tsx
@@ -1,10 +1,39 @@
 import "./App.css";
+import Button from "./components/Button";
 import Input from "./components/Input";
+import {useState} from "react";
+import Results from "./components/Results";
 
 function App() {
+  const [humanPrompt, setHumanPrompt] = useState("");
+
+  const [showResults, setShowResults] = useState(false);
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+
+    setShowResults(true);
+
+    console.log(humanPrompt);
+  }
+
   return (
     <div className="flex items-center justify-center h-screen">
-      <Input />
+      <div className="flex flex-col">
+        <div className="flex items-center">
+          <Input
+            value={humanPrompt}
+            f={(e: any) => setHumanPrompt(e.target.value)}
+          />
+          <Button onClick={handleSubmit} />
+        </div>
+
+        {showResults && (
+          <div>
+            <Results />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- my-vite-react-app/src/components/Input.tsx
+++ my-vite-react-app/src/components/Input.tsx
@@ -1,5 +1,5 @@
-export default function Input() {
+export default function Input({value, f}:any) {
     return (
-      <input className="h-24 w-96" placeholder="What would you like to learn about your spotify data?" />
+      <input value={value} onChange={f} className="h-24 w-96" placeholder="What would you like to learn about your spotify data?" />
     );
 }
