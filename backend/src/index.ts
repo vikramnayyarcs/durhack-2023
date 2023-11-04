78db808f6d336a680fdf3bf22d1a227b4e5463b9
maulish34 <c2045137@ncl.ac.uk>
2023-11-04 15:25:57 +0000

    Added DB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import {PrismaClient} from "@prisma/client";
 
 const app = express();
+
+
+type Row = {
+    id: number;
+    
+}
+
 
 app.use(express.json());
 app.use(cors());
@@ -10,8 +18,14 @@ app.get('/api/search', async (req, res) => {
     res.json({ message: 'success' });
 });
 
-app.listen(5030, () => {
-    console.log('Server running on localhost:5030');
+app.post('/api/search', async (req, res) => {
+   res.json({message: "put query here"});
+});
+
+
+
+app.listen(5001, () => {
+    console.log('Server running on localhost:5001');
 });
 
 export default app;
